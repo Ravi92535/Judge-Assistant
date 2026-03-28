6e2f6a253bc2c64cadde3588544f948a31e6fc3b
Ravindra  Prajapat <ravi@MacBook-Air-3.local>
2026-03-28 10:48:00 +0530

    App will run through main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+
+import subprocess
+import sys
+from src.ingest_rag import main as ingest_rag
+
 def main():
-    print("Hello from gandu!")
+    # First run rag pipline of Nation Docs
+    ingest_rag()
 
+    # Automatically start the Streamlit app
+    print("Starting Streamlit app...")
+    try:
+        subprocess.run([sys.executable, "-m", "streamlit", "run", "src/app.py"])
+    except KeyboardInterrupt:
+        print("\nStopping Streamlit app...")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
